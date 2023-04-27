038ba21befac191abbb05753ecdb89648055a759
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2023-04-27 16:32:13 +0400

    add: json ld

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,29 @@ import Anchor from '@/components/ui/Anchor'
 import Title from '@/components/ui/Title'
 import { FC } from 'react'
 
+let schema = {
+  '@context': 'https://schema.org',
+  '@type': 'Porfolio',
+  mainEntityOfPage: {
+    '@type': 'WebPage',
+    '@id': 'https://mehdiasadli.vercel.app'
+  },
+  headline: 'Software Dev. Portfolio',
+  datePublished: '2023-04-27',
+  author: [
+    {
+      '@type': 'Person',
+      name: 'Mehdi Asadli',
+      url: 'https://mehdiasadli.vercel.app/about'
+    }
+  ],
+  publisher: {
+    '@type': 'Person',
+    name: 'Mehdi Asadli'
+  },
+  description: 'This is a portfolio website of a software developer'
+}
+
 const Home: FC = ({}) => {
   return (
     <main className='text-text-l p-3 min-h-screen'>
@@ -13,6 +36,10 @@ const Home: FC = ({}) => {
       <p>
         You can read more about me on <Anchor label='about-me.txt' link='/about' /> file.
       </p>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
     </main>
   )
 }
